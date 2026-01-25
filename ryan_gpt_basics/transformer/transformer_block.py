03454d0aca509bfdff08dd6d0459c6673ae5e72d
Ryan Zhou <ryanawesome888@gmail.com>
2026-01-24 22:57:47 -0500

    Refactored code

--- ryan_gpt_basics/transformer/transformer_block.py
+++ ryan_gpt_basics/transformer/transformer_block.py
@@ -20,24 +20,6 @@ class TransformerBlock(torch.nn.Module):
 
 
     def forward(self, x: torch.Tensor, token_positions: torch.Tensor | None = None) -> torch.Tensor:
-        """
-        Apply the Transformer block to the input tensor.
-        
-        x: torch.Tensor Input tensor of shape (batch_size, seq_len, d_model)
-        token_positions: torch.Tensor | None Tensor of shape (batch_size, seq_len) indicating the position of each token
-        """
-        # Apply RMSNorm
-        x_norm1 = self.rmsnorm1(x)
-        # Apply Multi-Head Self-Attention
-        mha_out = self.mha(x_norm1, token_positions)
-        # Add residual connection
-        x_res1 = x + mha_out
-
-        # Apply RMSNorm
-        x_norm2 = self.rmsnorm2(x_res1)
-        # Apply SwiGLU Feed-Forward Network
-        ff_out = self.ffn(x_norm2)
-        # Add residual connection
-        y = x_res1 + ff_out
-
-        return y
+        x = x + self.mha(self.rmsnorm1(x), token_positions)
+        x = x + self.ffn(self.rmsnorm2(x))
+        return x
